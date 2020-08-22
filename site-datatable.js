36664eece1ed797dd7c7fac1649065fbe203a95d
socraticDevBlog <maximebonin@outlook.com>
2020-08-21 22:39:25 -0400

    added ellipsis datatable

--- site-datatable.js
+++ site-datatable.js
@@ -1,3 +1,12 @@
-$(document).ready( function () {
-    $('#table_id').DataTable();
-} );
+$(document).ready(function () {
+
+    // inject ellipsis plugin into DataTable's api
+    $.fn.dataTable.render.ellipsis = ellipsis;
+
+    $('#table_id').DataTable({
+        columnDefs: [{
+            targets: 1,
+            render: $.fn.dataTable.render.ellipsis(20, true)
+        }]
+    });
+});
